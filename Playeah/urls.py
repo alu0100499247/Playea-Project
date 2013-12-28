1a408a5dad154d3aad3974b127754ce5289ba4b2
Cristhian Garcia <alu0100499247@ull.edu.es>
2013-12-28 00:11:41 +0000

    Agregado servidor de medios y css basico

--- Playeah/urls.py
+++ Playeah/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+import settings
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -16,4 +17,5 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^',include('Playeah.apps.home.urls')),
     url(r'^',include('Playeah.apps.Playeah.urls')),
+    url(r'^media/(?P<path>.*)$','django.views.static.serve',{'document_root':settings.MEDIA_ROOT}),
 )
