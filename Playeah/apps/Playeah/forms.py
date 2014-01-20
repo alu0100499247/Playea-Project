83686cf37d16ba7b93a440c8466b47683db2d8d8
Cristhian Garcia <alu0100499247@ull.edu.es>
2014-01-20 18:14:56 +0000

    Actualizados formularios de agregar y editar categorias y etiquetas de playas

--- Playeah/apps/Playeah/forms.py
+++ Playeah/apps/Playeah/forms.py
@@ -1,10 +1,7 @@
 from django import forms
+from Playeah.apps.Playeah.models import playa
 
-class addPlayaForm(forms.Form):
-	nombre 		= forms.CharField(widget=forms.TextInput())
-	ubicacion 	= forms.CharField(widget=forms.TextInput())
-	descripcion = forms.CharField(widget=forms.Textarea())
-	imagen		= forms.ImageField(required=False)
-
-	def clean(self):
-		return self.cleaned_data
+class addPlayaForm(forms.ModelForm):
+	class Meta:
+		model 	= playa
+		exclude = {'status'}
--- Playeah/apps/Playeah/views.py
+++ Playeah/apps/Playeah/views.py
@@ -5,53 +5,31 @@ from Playeah.apps.Playeah.models import playa
 from django.http import HttpResponseRedirect
 
 def add_playa_view(request):
-	info = "Inicializando"
-	if request.user.is_authenticated():
-		if request.method == "POST":
-			form = addPlayaForm(request.POST,request.FILES)
-			if form.is_valid():
-				nombre = form.cleaned_data['nombre']
-				ubicacion = form.cleaned_data['ubicacion']
-				descripcion = form.cleaned_data['descripcion']
-				imagen = form.cleaned_data['imagen'] 	# Esto se obtiene con el request.FILES
-				p = playa()
-				if imagen:	# Generamos una validacion
-					p.imagen = imagen
-				p.nombre 		= nombre
-				p.ubicacion 	= ubicacion
-				p.descripcion 	= descripcion
-				p.status = True
-				p.save()	# Guardamos la informacion
-				info = "La playa ha sido almacenada correctamente."
-			else:
-				info = "Informacion con datos incorrectos."
-		form = addPlayaForm()
-		ctx = {'form':form,'informacion':info}
-		return render_to_response('Playeah/addPlaya.html',ctx,context_instance=RequestContext(request))
+	if request.method == "POST":
+		form = addPlayaForm(request.POST,request.FILES)
+		if form.is_valid():
+			add = form.save(commit=False)
+			add.status = True
+			add.save()
+			form.save_m2m() 	# Guarda los ManyToMany
+			return HttpResponseRedirect('/playa/%s/'%add.id)
 	else:
-		return HttpResponseRedirect('/')
+		form = addPlayaForm()
+	ctx = {'form':form}
+	return render_to_response('Playeah/addPlaya.html',ctx,context_instance=RequestContext(request))
 
 def edit_playa_view(request, id_playa):
-	p = playa.objects.get(id=id_playa)
+	playita = playa.objects.get(pk=id_playa)
 	if request.method == "POST":
-		form = addPlayaForm(request.POST, request.FILES)
+		form = addPlayaForm(request.POST,request.FILES,instance=playita)
 		if form.is_valid():
-			nombre 		= form.cleaned_data['nombre']
-			ubicacion 	= form.cleaned_data['ubicacion']
-			descripcion = form.cleaned_data['descripcion']
-			imagen 		= form.cleaned_data['imagen']
-			p.nombre 		= nombre
-			p.ubicacion 	= ubicacion
-			p.descripcion 	= descripcion
-			if imagen:		# Verificamos la imagen
-				p.imagen 	= imagen
-			p.save()
-			return HttpResponseRedirect('/playa/%s'%p.id)
-	if request.method == "GET":
-		form = addPlayaForm(initial={
-			'nombre'		:p.nombre,
-			'ubicacion'		:p.ubicacion,
-			'descripcion'	:p.descripcion,
-			})
-	ctx = {'form':form,'playa':p}
+			edit = form.save(commit=False)
+			edit.status = True
+			edit.save()
+			form.save_m2m()
+			return HttpResponseRedirect('/playa/%s/'%edit.id)
+	else:
+		form = addPlayaForm(instance=playita)
+	ctx = {'form':form}
 	return render_to_response('Playeah/editPlaya.html',ctx,context_instance=RequestContext(request))
+
