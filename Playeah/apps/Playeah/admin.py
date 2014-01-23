f16e2fe99be140effc0c696ebae150ce3ee9e48a
Cristhian Garcia <alu0100499247@ull.edu.es>
2014-01-23 20:30:05 +0000

    Modificada apariencia

--- Playeah/apps/Playeah/admin.py
+++ Playeah/apps/Playeah/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from Playeah.apps.Playeah.models import usuario, playa, categoria, deporte, cercania, bandera 
 
-admin.site.register(usuario)
 admin.site.register(playa)
 admin.site.register(categoria)
 admin.site.register(deporte)
---
+++ Playeah/apps/context_processors.py
@@ -0,0 +1,6 @@
+import django
+
+def my_processor(request):
+	context = {"django_version":django.get_version(),
+	}
+	return context
